32dbe850c65dcaca86ef336649d628b54f74fdec
Valentino-git2305 <v.molinelli@alumno.um.edu.ar>
2025-04-15 16:09:36 -0300

    3da correccion de implementacion para casos no palindromos

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -16,5 +16,6 @@ class TestPalindrome(unittest.TestCase):
         self.assertFalse(is_palindrome("hello"))
         self.assertFalse(is_palindrome("python"))
         self.assertFalse(is_palindrome("This is not a palindrome"))
+
 if __name__ == '__main__':
     unittest.main()
